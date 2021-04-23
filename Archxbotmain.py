c720d4e1ac9c14446ae300fb0ad0fa9ff6cc06eb
Archivicore <mischzt0199@gmail.com>
2021-04-23 11:13:17 +0700

    master

---
+++ Archxbotmain.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/Archivicore/ArchX-86 /root/Archxbot && mkdir /root/Archxbot/bin/ && cd /root/Archxbot/ && chmod +x /usr/local/bin/* && python3 -m Archxbot"
+)
