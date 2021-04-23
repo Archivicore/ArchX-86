08f91477436c936ce33d0103b505fbf83632fce6
Archivicore <82773834+Archivicore@users.noreply.github.com>
2021-04-23 23:34:31 +0700

    Update __init__.py

--- Archxbot/modules/__init__.py
+++ Archxbot/modules/__init__.py
@@ -52,7 +52,7 @@ else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING FRIDAYUBOT STATS ✘\n"
+    f"✘ ARCHXBOT-86 STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {amiusingsudo} \n"
