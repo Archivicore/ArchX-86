80822d7261c400baa013fc85edaa420e46920f71
Archivicore <82773834+Archivicore@users.noreply.github.com>
2021-05-25 10:35:56 +0700

    Update button.py

--- Archxbot/modules/button.py
+++ Archxbot/modules/button.py
@@ -6,7 +6,7 @@ import re
 
 from telethon import Button
 
-from Archxbot import TG_BOT_USER_NAME_BF_HER
+from .Config import TG_BOT_USER_NAME_BF_HER
 
 # regex obtained from:
 # https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
